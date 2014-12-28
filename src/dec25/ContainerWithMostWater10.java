db3c9e5c94e2bca3b8a660dd59aaedec1883c41b
E21cQ1 <idealqipeng@gmail.con>
2014-12-28 08:54:06 -0800

    Unsolved

---
+++ src/dec25/ContainerWithMostWater10.java
@@ -0,0 +1,26 @@
+package dec25;
+
+/**
+ * Greedy Algorithm
+ * @author Paul Qi
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * Note: You may not slant the container. (height * width = container)
+ * 
+ * brute force O(n^2)
+ * 
+ * I don't find a good way to divide and conquer, because I cannot find a good way to combine the result
+ * I try to order the height based on the height, still I cannot find a good way to reduct from O(n^2) to ??
+ */
+
+public class ContainerWithMostWater10 {
+    public static int maxArea(int[] height) {
+    	
+    	return 0;
+    }
+	
+	public static void main(String[] args){
+		
+	}
+}
