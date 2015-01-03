96f2b711d1d130e80cd7cb851ef64404fb443eb7
E21cQ1 <idealqipeng@gmail.con>
2015-01-03 09:20:24 -0800

    Unsolved

---
+++ src/jan2/GenerateParentheses22.java
@@ -0,0 +1,21 @@
+package jan2;
+
+import java.util.List;
+
+/**
+ * @author Paul Qi
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * For example, given n = 3, a solution set is:
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
+ */
+
+public class GenerateParentheses22 {
+    public static List<String> generateParenthesis(int n) {
+        
+    	return null;
+    }
+    
+    public static void main(String[] s){
+    	
+    }
+}
