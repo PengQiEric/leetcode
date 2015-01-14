9238fce943675c37efaa67600c6d35f596a132d7
E21cQ1 <idealqipeng@gmail.con>
2015-01-13 19:05:50 -0800

    Unsolved Median of two array

--- src/Dec22/MedianOfTwoSortedArray1.java
+++ src/Dec22/MedianOfTwoSortedArray1.java
@@ -1,6 +1,7 @@
 package Dec22;
 
 /**
+ * Unsolved
  * @author Paul Qi
  * There are two sorted arrays A and B of size m and n respectively. 
  * Find the median of the two sorted arrays. 
@@ -10,14 +11,31 @@ package Dec22;
 public class MedianOfTwoSortedArray1 {
 	
 	public static void main(String[] args){
-		double result = findMedianSortedArrays(new int[]{1,2,3,4,5,6,7,20},new int[]{9,10,11,12,13,14,15,16});
+		double result = findMedianSortedArrays_try1(new int[]{1,2,3,4,5,6,13,15},new int[]{7,8,11,12,13,14,15,16});
 		System.out.println(result);
 	}
 
-    public static double findMedianSortedArrays(int A[], int B[]) {
+	public static double findMedianSortedArrays(int A[], int B[]){
+		int length = (A.length + B.length)/2;
+		int Astart = 0, Bstart = 0;
+		while(length<=1){
+			if(A[Astart]<B[Bstart]){
+				
+			}
+		}
+		
+		return 0;
+	}
+	
+	
+	/**
+	 * try 1, doesn't work if the two median element are in the same element
+	 * and it must requires the two array has the same length
+	 */
+	public static double findMedianSortedArrays_try1(int A[], int B[]) {
     	int midA, midB;
-    	int startA = 0, startB = 0, endA = A.length, endB = B.length;
-    	while(startA<endA && startB<endB){
+    	int startA = 0, startB = 0, endA = A.length-1, endB = B.length-1;
+    	while(startA<=endA && startB<=endB){
         	midA = (startA+endA)/2;
         	midB = (startB+endB)/2;
         	if(A[midA]<B[midB]){ // drop half of the larger part of B, drop half of the small part of A
@@ -30,7 +48,8 @@ public class MedianOfTwoSortedArray1 {
         	}
     	}
     	System.out.println(startA+" "+startB);
-    	return (A[startA]+B[startB])/2.0;
+//    	return (A[startA]+B[startB])/2.0;
+    	return A[startA]>B[startB]?B[startB]:A[startA];
     }
     
 }
