72b26b9f09a8045fe58a4896f3ae848653a61a14
E21cQ1 <idealqipeng@gmail.con>
2015-01-17 15:37:50 -0800

    O(1) space for in-order traversal

---
+++ src/january17/RecoverBinarySearchTree99_MorrisTraversal.java
@@ -0,0 +1,47 @@
+package january17;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import aTool.TreeNode;
+
+public class RecoverBinarySearchTree99_MorrisTraversal {
+
+	// traversal the tree with O(1) space
+	// basically it is partial threaded Binary Tree 
+	// 	where the right null pointer will point to the successor
+    public List<Integer> inorderTraversal(TreeNode root) {
+    	List<Integer> results = new LinkedList<Integer>();
+    	TreeNode cur = root;
+    	while(cur!=null){
+    		if(cur.left == null){
+    			results.add(cur.val);
+    			cur = cur.right;
+    		}
+    		else{
+    			TreeNode prev = cur.left;
+    			while(prev.right!=null && prev.right!= cur){
+    				prev = prev.right;  
+    			}
+    			
+    			if(prev.right == null){ // the right most node in the current left subtree
+    				prev.right = cur;
+    				cur = cur.left;
+    			}
+    			
+    			if(prev.right == cur){ // finish traversal the left subtree
+    				results.add(cur.val);
+    				prev.right = null;
+    				cur = cur.right;
+    			}
+    		}
+    	}
+    	
+    	return results;
+    }
+    
+    // do next time
+    public void recoverTree(TreeNode root) {
+    	//
+    }
+}
