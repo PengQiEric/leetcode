6a7937941ce77f4bdcaf1fa990006e64191cebc1
E21cQ1 <idealqipeng@gmail.con>
2015-01-19 09:06:36 -0800

    Typical two pointers problem

---
+++ src/january19_list/LinkedListCycle141.java
@@ -0,0 +1,40 @@
+package january19_list;
+
+import aTool.ListNode;
+
+/**
+ * @author Paul Qi
+ * Given a linked list, determine if it has a cycle in it.
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+
+public class LinkedListCycle141 {
+    
+	public boolean hasCycle(ListNode head) {
+		boolean first = true;
+		ListNode fast = head, slow = head;
+		while((fast!=null && fast.next!=null) && (first || (!slow.equals(fast) ))){ // slow!=fast
+			first = false;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		if(fast == null || fast.next==null){
+			return false;
+		}
+		else{
+			return true;
+		}
+    }
+	
+	public static void main(String[] args){
+		ListNode n0 = new ListNode(0);
+		ListNode n1 = new ListNode(1);
+		n0.next = n1;
+		n1.next = n0;
+		
+		LinkedListCycle141 test = new LinkedListCycle141();
+		System.out.println(test.hasCycle(n0));
+	}
+}
