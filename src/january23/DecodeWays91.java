fd953b4515e4998745e58dc4d8d5aa3278fd7f0d
E21cQ1 <idealqipeng@gmail.con>
2015-01-23 21:18:58 -0800

    DP

---
+++ src/january23/DecodeWays91.java
@@ -0,0 +1,68 @@
+package january23;
+
+
+/**
+ * @author PAUL QI
+ * 
+ *   A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+	'A' -> 1
+	'B' -> 2
+	...
+	'Z' -> 26
+	Given an encoded message containing digits, determine the total number of ways to decode it.
+	
+	For example,
+	Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+	
+	The number of ways decoding "12" is 2.
+ */
+
+public class DecodeWays91 {
+    // bottom up
+    public int numDecodings(String s) {
+//    	if(s.indexOf("0")>=0){
+//    		return 0;	// test case: "01" 0
+//    	}
+    	if(s.length() == 0){
+    		return 0;
+    	}
+    	int[] results = new int[s.length()];
+    	results[0] = s.charAt(0) == '0'?0:1;
+    	
+    	for(int i=1; i<s.length(); i++){
+    		if(s.charAt(i) == '0'){
+    			if(s.charAt(i-1) == '1' || s.charAt(i-1) == '2'){
+    				if(i-2>=0){
+    					results[i] = results[i-2];
+    				}
+    				else{
+        				results[i] = 1;
+    				}
+    			}
+    			else{
+    				return 0;
+    			}
+    		}
+    		else if(Integer.parseInt(s.substring(i-1,i+1))<=26 && Integer.parseInt(s.substring(i-1,i+1))>=11){
+    			if(i-2>=0){
+    				results[i] = results[i-1]+results[i-2];
+    			}
+    			else{
+    				results[i] = results[i-1]+1;
+    			}
+    		}
+    		else{
+    			results[i] = results[i-1];
+    		}
+    	}
+    	
+    	return results[s.length()-1];
+    }
+    
+    public static void main(String[] args){
+    	DecodeWays91 test = new DecodeWays91();
+    	System.out.println(test.numDecodings("012"));
+    	System.out.println(test.numDecodings("230"));
+    }
+}
