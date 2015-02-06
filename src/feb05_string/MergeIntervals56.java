e6d22f0425c5c22c26b28154e3a48a4624d9513b
E21cQ1 <idealqipeng@gmail.con>
2015-02-05 16:33:22 -0800

    Add Comments

--- src/feb05_string/MergeIntervals56.java
+++ src/feb05_string/MergeIntervals56.java
@@ -42,12 +42,26 @@ public class MergeIntervals56 {
     	return results;
     }
     
+    /*
+     *  Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+		You may assume that the intervals were initially sorted according to their start times.
+		
+		Example 1:
+		Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+		
+		Example 2:
+		Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+		
+		This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+     */
+    
+    // in-place O(n) solution if the List is LinkedList
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-    	Collections.sort(intervals, new MyComparator());
+//    	Collections.sort(intervals, new MyComparator());
     	int i=0;
     	for(;i<intervals.size();){
     		Interval temp = intervals.get(i);
-    		if(newInterval.end<temp.start){
+    		if(newInterval.end<temp.start){	// only here use the sort attribute
     			break;
     		}
     		else if(newInterval.end>=temp.start && newInterval.end<=temp.end
